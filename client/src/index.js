71a628184e195a0e1f7ac1ac157c9de6e6e309d3
MaxiV95 <maximilianovanmegroot@gmail.com>
2023-06-07 15:26:11 -0300

    actualización-llamada-dotenv

--- client/src/index.js
+++ client/src/index.js
@@ -10,7 +10,7 @@ import store from "./redux/store";
 
 require('dotenv').config();
 
-axios.defaults.baseURL = process.env.URL_SERVER;
+axios.defaults.baseURL = process.env.REACT_APP_URL_SERVER;
 
 ReactDOM.render(
   <Provider store={store}> {/* Estado global */}
@@ -26,4 +26,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals(console.log);
+reportWebVitals();
